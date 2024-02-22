b650720fb8d521c455af3572756e82a9a9444428
Kirill <vkashe1337@gmail.com>
2024-02-22 16:19:37 +0300

    Исправлены замечания

--- src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
@@ -1,8 +1,12 @@
 package ru.kata.spring.boot_security.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.FileCopyUtils;
+import org.springframework.util.ResourceUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,7 +16,11 @@ import ru.kata.spring.boot_security.demo.entity.User;
 import ru.kata.spring.boot_security.demo.repository.RoleRepository;
 import ru.kata.spring.boot_security.demo.service.UserService;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 public class AdminController {
@@ -27,11 +35,24 @@ public class AdminController {
     }
 
     @GetMapping("/admin")
-    public String showUsers(Model model) {
+    public String showUsers(Model model, Authentication authentication) throws IOException {
         List<User> userList = userService.getAllUser();
+        List<Role> userRoleList = roleRepository.findAll();
+
+        File file = ResourceUtils.getFile("classpath:all-users.html");
+        String content = new String(FileCopyUtils.copyToByteArray(file));
+        model.addAttribute("activeWindowName","Admin panel");
+        model.addAttribute("userRole",authentication.getAuthorities()
+                .stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.toList()));
+        model.addAttribute("userName", authentication.getName());
         model.addAttribute("all_user", userList);
-        return "all-users";
+        model.addAttribute("roles", userRoleList);
+        model.addAttribute("content", content);
+        return "Layout";
     }
+
 
     @GetMapping("/admin/addNewUser")
     public String addNewUser(Model model) {
