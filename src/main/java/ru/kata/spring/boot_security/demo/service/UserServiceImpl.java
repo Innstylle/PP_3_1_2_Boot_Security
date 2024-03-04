86f3b4a81258d3bdfba29eca54bee6d7c4c1b226
Kirill <vkashe1337@gmail.com>
2024-03-04 14:06:07 +0300

    //

--- src/main/java/ru/kata/spring/boot_security/demo/service/UserServiceImpl.java
+++ src/main/java/ru/kata/spring/boot_security/demo/service/UserServiceImpl.java
@@ -1,6 +1,5 @@
 package ru.kata.spring.boot_security.demo.service;
 
-import com.sun.istack.NotNull;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
