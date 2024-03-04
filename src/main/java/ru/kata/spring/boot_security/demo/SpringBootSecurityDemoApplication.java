7b7c275961d556a95f5081e7bf756fc68d5b73f3
Kirill <vkashe1337@gmail.com>
2024-03-04 16:18:41 +0300

    //

--- src/main/java/ru/kata/spring/boot_security/demo/SpringBootSecurityDemoApplication.java
+++ src/main/java/ru/kata/spring/boot_security/demo/SpringBootSecurityDemoApplication.java
@@ -2,12 +2,43 @@ package ru.kata.spring.boot_security.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import ru.kata.spring.boot_security.demo.entity.Role;
+import ru.kata.spring.boot_security.demo.entity.User;
+import ru.kata.spring.boot_security.demo.repository.RoleRepository;
+import ru.kata.spring.boot_security.demo.service.UserService;
+
+import javax.annotation.PostConstruct;
+import java.util.Arrays;
+import java.util.List;
 
 @SpringBootApplication
 public class SpringBootSecurityDemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootSecurityDemoApplication.class, args);
-	}
+    private static RoleRepository roleRepository;
+    private static UserService userService;
 
+    public SpringBootSecurityDemoApplication(RoleRepository roleRepository, UserService userService) {
+        SpringBootSecurityDemoApplication.roleRepository = roleRepository;
+        SpringBootSecurityDemoApplication.userService = userService;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootSecurityDemoApplication.class, args);
+    }
+
+    @PostConstruct
+    public static void initialize() {
+        List<Role> defaultRoles = Arrays.asList(
+                new Role(Long.getLong("1"), "ROLE_ADMIN", null),
+                new Role(Long.getLong("2"), "ROLE_USER", null)
+        );
+
+        for (Role role : defaultRoles) {
+            if (!roleRepository.existsByName(role.getName())) {
+                roleRepository.save(role);
+            }
+        }
+         userService.saveUser(new User(null, "user", "password",
+                "user", "user@gmail.com", 10, defaultRoles));
+    }
 }
